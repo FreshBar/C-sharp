c52bf07587af9a05dfce47aacf88e24ca2836587
Elena <e.l.e.n.a_68@mail.ru>
2019-04-11 16:41:28 +0400

    Разработан перевод с 10-тичной системы счисления в 2-ичную.

---
+++ transfer/transfer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Введите число: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            int i;
+            for (i=1; i <= num; i *= 2) ;
+            i /= 2;
+            for (; i >= 1; i /= 2)
+            {
+                if (num - i >= 0)
+                {
+                    Console.Write("1");
+                    num = num - i;
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+            }
+
+            Console.Read();
+           
+        }
+    }
+}
