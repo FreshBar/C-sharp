6c1f7c6ea3f7adeb9f7a6314af7c94c59e3a1f1d
Elena <e.l.e.n.a_68@mail.ru>
2019-04-12 18:57:17 +0400

    Реализован перевод из 10 системы счисления в любую от 2 до 36

--- transfer/transfer/Program.cs
+++ transfer/transfer/Program.cs
@@ -8,30 +8,42 @@ namespace transfer
 {
     class Program
     {
+        static char give_num_symb(int num)
+        {
+            return (num < 10) ? (char)(num + '0') : (char)('A' + num - 10);
+        }
+
+
         static void Main(string[] args)
         {
+            int num_base;
             int num;
             Console.Write("Введите число: ");
             num = Convert.ToInt32(Console.ReadLine());
-            
-            int i;
-            for (i=1; i <= num; i *= 2) ;
-            i /= 2;
-            for (; i >= 1; i /= 2)
-            {
-                if (num - i >= 0)
-                {
-                    Console.Write("1");
-                    num = num - i;
-                }
-                else
-                {
-                    Console.Write("0");
-                }
-            }
+            Console.Write("Введите систему счисления от 2 до 36: ");
+            num_base = Convert.ToInt32(Console.ReadLine());
 
+            if (num_base < 2 || num_base > 36)
+            {
+                Console.WriteLine("Неправильная система счисления!");
+                Console.Read();
+                return;
+            }
+            int i;
+            for (i = 1; i <= num; i *= num_base) ;
+            i /= num_base;
+            for (; i >= 1; i /= num_base)
+            {
+                int k;
+                for (k = num_base - 1; k > 0; k--)
+                {
+                    if (k * i <= num)
+                        break;
+                }
+                num -= i * k;
+                Console.Write((char)(give_num_symb(k)));
+            }
             Console.Read();
-           
         }
     }
 }
